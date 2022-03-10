c3ff5cb6732d03c17523d0a9f39ca2a90b0f6f66
Kappa2727 <98384727+Kappa2727@users.noreply.github.com>
2022-03-10 23:55:39 +0100

    Add files via upload

--- modele.py
+++ modele.py
@@ -12,15 +12,23 @@ définition des variables globals qui seront utilisée pour le jeu
 """
 global main
 main= Tk()
-global CAN_SIZE #la taille des canvas utilisée pour le jeu
-global CELL_SIZE #la taille des cellules du canva (cad: taille des carrès de la grille )
-CAN_SIZE = 405 #360, car c'est divisible par 9 et que c'est une taille convenable
-CELL_SIZE = CAN_SIZE / 9 # 9, parce qu'il y'a 9 lignes et 9 colones dans une grille de bataille navale, donc on divise par 9 la taille du canva utilisé pour afficher la grille
+global CANVA_TAIL #la taille des canvas utilisée pour le jeu
+global CASE_TAIL #la taille des cellules du canva (cad: taille des carrès de la grille )
+CANVA_TAIL = 405 #360, car c'est divisible par 9 et que c'est une taille convenable
+CASE_TAIL = CANVA_TAIL / 9 # 9, parce qu'il y'a 9 lignes et 9 colones dans une grille de bataille navale, donc on divise par 9 la taille du canva utilisé pour afficher la grille
+
+global frame1 #la fenêtre utilisé pour le menu du jeu
+global frame2
+global frame3
+frame1= Frame(main, width=1980,height=1080,bg="black")
+frame2= Frame(main, width=1980,height=1080,bg="red")
+frame3= Frame(main, width=1980,height=1080,bg="black")
+
 
 #-------------------------------------------------------------------------------------------------------------------------------
 
 """
-etablissement des variables globals pour le canva/grille allié et ennemi
+etablissement des variables globals pour le canva/grille allié et ennemi mais aussi pour le placement des bateaux
 """
 global CAN_ALLIE
 global GRILLE_ALLIE
@@ -29,6 +37,14 @@ GRILLE_ALLIE = [[0 for i in range(9)] for i in range(9)] #le nombre de lignes et
 global CAN_ENNEMI
 global GRILLE_ENNEMI
 GRILLE_ENNEMI = [[0 for i in range(9)] for i in range(9)] #le nombre de lignes et de colones qu'auront la grille de l'ennemi
+
+global CAN_PlacementBateaux
+global GRILLE_PlacementBateaux
+GRILLE_PlacementBateaux = [[0 for i in range(9)] for i in range(9)] #le nombre de lignes et de colones qu'auront la grille pour le placement des bateaux
+global CANVA_TAIL_Placement #la taille des canvas utilisée pour la fenêtre du placement des bateaux
+global CASE_TAIL_Placement #la taille des cellules du canva pour le placement des bateaux (cad: taille des carrès de la grille )
+CANVA_TAIL_Placement = 540 #540, car c'est divisible par 9 et que c'est une taille convenable pour le placement des bateaux
+CASE_TAIL_Placement = CANVA_TAIL_Placement / 9 # 9, parce qu'il y'a 9 lignes et 9 colones dans une grille de bataille navale, donc on divise par 9 la taille du canva utilisé pour afficher la grille
 
 #-------------------------------------------------------------------------------------------------------------------------------
 
--- view.py
+++ view.py
@@ -8,27 +8,49 @@ Created on Wed Mar  9 14:20:26 2022
 from modele import *
 
 
-"""
-fonction permettant d'établir le plateau de jeu avec les canvas contenant les grilles
-"""
 def etablirplateau():
-    fond= Canvas(main,width=1980,height=1080,bg="black") #le fond du jeu
+    fond= Canvas(frame3,width=1980,height=1080,bg="black") #le fond du jeu
     fond.place(relx=0.5,rely=0.5,anchor=CENTER)
     
     
-    CAN_ALLIE = Canvas(main,width=CAN_SIZE,height=CAN_SIZE,bg="white",highlightthickness=0) #highlightthickness=0 permet d'enlever les ombres (plus esthétique)
+    CAN_ALLIE = Canvas(frame3,width=CANVA_TAIL,height=CANVA_TAIL,bg="white",highlightthickness=0) #highlightthickness=0 permet d'enlever les ombres (plus esthétique)
     CAN_ALLIE.place(relx=0.5,rely=0.25,anchor=CENTER) #la grille allie est place parallèlement a la grille ennemi sur l'axe horizontale 
     for row in range(len(GRILLE_ALLIE)):
         for col in range(len(GRILLE_ALLIE[row])):
-            CAN_ALLIE.create_rectangle(col * CELL_SIZE,row * CELL_SIZE,col * CELL_SIZE + CELL_SIZE,row * CELL_SIZE + CELL_SIZE,outline="black") #utilisation de la taille des cellules pour definir le point d'origine en haut a gauche, vers le point d'arrivée en bas a droite
+            CAN_ALLIE.create_rectangle(col * CASE_TAIL,row * CASE_TAIL,col * CASE_TAIL + CASE_TAIL,row * CASE_TAIL + CASE_TAIL,outline="black") #utilisation de la taille des cellules pour definir le point d'origine en haut a gauche, vers le point d'arrivée en bas a droite
                 
 
-    CAN_ENNEMI = Canvas(main,width=CAN_SIZE,height=CAN_SIZE,bg="white",highlightthickness=0)
+    CAN_ENNEMI = Canvas(frame3,width=CANVA_TAIL,height=CANVA_TAIL,bg="white",highlightthickness=0)
     CAN_ENNEMI.place(relx=0.5,rely=0.75,anchor=CENTER)
     for row in range(len(GRILLE_ENNEMI)):
         for col in range(len(GRILLE_ENNEMI[row])):
-            CAN_ENNEMI.create_rectangle(col * CELL_SIZE,row * CELL_SIZE,col * CELL_SIZE + CELL_SIZE,row * CELL_SIZE + CELL_SIZE,outline="black")
+            CAN_ENNEMI.create_rectangle(col * CASE_TAIL,row * CASE_TAIL,col * CASE_TAIL + CASE_TAIL,row * CASE_TAIL + CASE_TAIL,outline="black")
+            
+def OuvrirPlacementBateaux():
+    frame1.destroy() #permet de détruire la frame1, c'est a dire le menu du jeu
+    frame2.pack() #permet de positionner la frame2 en avant, c'est a dire la fenêtre du placement des Bateaux
+    PlacementBateaux() #appel la fonction permettant de placer les bateaux sur ça grille avant de commencer la partie
+        
+def menu():
+    frame1.pack() #permet de positionner la frame2 en avant, c'est a dire le menu du jeu
+    Boutonjouer= Button(frame1, width=10, height=2, command=OuvrirPlacementBateaux) #un bouton jouer qui permet d'appeler la fonction OuvrirPlacementBateaux
+    BoutonQuitter= Button(frame1, width=10, height=2, command=main.destroy) #un bouton qui permet de  fermer le jeu
+    Boutonjouer.place(relx=0.5, rely=0.40, anchor=CENTER)
+    BoutonQuitter.place(relx=0.5, rely=0.60, anchor=CENTER)
+    
+def OuvrirLeCombat():
+    frame2.destroy() #permet de détruire la frame2, c'est a dire fenêtre du placement des Bateaux
+    frame3.pack() #permet de positionner la frame3 en avant, c'est a dire la zone de combat navale
+    etablirplateau() #appel la fonction permettant d'établir le plateau de la zone de combat navale
 
-
+def PlacementBateaux():
+    CAN_PlacementBateaux = Canvas(frame2,width=CANVA_TAIL_Placement,height=CANVA_TAIL_Placement,bg="white",highlightthickness=0) #canva utilisé pour le placement des bateaux
+    CAN_PlacementBateaux.place(relx=0.25,rely=0.5,anchor=CENTER)
+    for row in range(len(GRILLE_PlacementBateaux)):
+        for col in range(len(GRILLE_PlacementBateaux[row])):
+            CAN_PlacementBateaux.create_rectangle(col * CASE_TAIL_Placement,row * CASE_TAIL_Placement,col * CASE_TAIL_Placement + CASE_TAIL_Placement,row * CASE_TAIL_Placement + CASE_TAIL_Placement,outline="black") #grille du placement des bateaux
+    BoutonCommencer= Button(frame2, width=10, height=2, command=OuvrirLeCombat) #un bouton qui permet d'appeler la fonction OuvrirLeCombat
+    BoutonCommencer.place(relx=0.5,rely=0.75,anchor=CENTER)
+   
 def show():
-    etablirplateau()
+    menu()
