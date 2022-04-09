59044228277c36ef1245543b010343794bac9180
Kappa2727 <98384727+Kappa2727@users.noreply.github.com>
2022-04-09 18:33:37 +0200

    Add files via upload

--- controller.py
+++ controller.py
@@ -3,5 +3,5 @@ from view import *
 
 #main.state("zoomed") #permet d'avoir la fenetre du jeu en fullscreen d'ai le lancement
 main.title("Bataille Navalle")
-menu() 
-main.mainloop()  
+menu()
+main.mainloop()
--- view.py
+++ view.py
@@ -8,63 +8,75 @@ Created on Wed Mar  9 14:20:26 2022
 from modele import *
 import random
 import time
+import pygame
 
 def etablirplateau():
     global CAN_ALLIE
     global CAN_ENNEMI
-    
-    fond= Canvas(frame3,width=1980,height=1080,bg="black") #le fond du jeu
-    can_full_eta= Canvas(frame3,width=288,height=162, bg="blue")
-    fond.place(relx=0.5,rely=0.5,anchor=CENTER)
-    can_full_eta.place(relx=0.75, rely=0.5, anchor=CENTER)
-    global GRILLE_PlacementBateaux
 
-    
+    fond= Canvas(frame3,width=1980,height=1080,bg="black") #le fond du jeu
+    fond.place(relx=0.5,rely=0.5,anchor=CENTER)
+    global GRILLE_PlacementBateaux
+    global compt1allie
+    global imgBoutqframe1
+    pygame.mixer.stop
+    pygame.mixer.music.load("bruitcombat.mp3")
+    pygame.mixer.music.set_volume(0.7)
+    pygame.mixer.music.play(loops=-1)
+
+
     CAN_ALLIE = Canvas(frame3,width=CANVA_TAIL,height=CANVA_TAIL,bg="white",highlightthickness=0)
     CAN_ENNEMI = Canvas(frame3,width=CANVA_TAIL,height=CANVA_TAIL,bg="white",highlightthickness=0)
     CAN_ALLIE.place(relx=0.5,rely=0.25,anchor=CENTER) #la grille allie est place parallèlement a la grille ennemi sur l'axe horizontale
     CAN_ENNEMI.place(relx=0.5,rely=0.75,anchor=CENTER)
-     
+
     for row in range(len(GRILLE_ALLIE)):
         for col in range(len(GRILLE_ALLIE[row])):
             CAN_ALLIE.create_rectangle(col * CASE_TAIL,row * CASE_TAIL,col * CASE_TAIL + CASE_TAIL,row * CASE_TAIL + CASE_TAIL,outline="black", tags="a2" + str(col) + str(row)) #utilisation de la taille des cellules pour definir le point d'origine en haut a gauche, vers le point d'arrivée en bas a droite
-    
+
     for l in range(len(GRILLE_PlacementBateaux)):
         for c in range(len(GRILLE_PlacementBateaux[l])):
             if GRILLE_PlacementBateaux[c][l]==1:
-                CAN_ALLIE.create_rectangle(CASE_TAIL*c,CASE_TAIL*l,(CASE_TAIL*c)+CASE_TAIL,(CASE_TAIL*l)+CASE_TAIL, fill="black", tags="a" + str(c) + str(l))  
+                CAN_ALLIE.create_rectangle(CASE_TAIL*c,CASE_TAIL*l,(CASE_TAIL*c)+CASE_TAIL,(CASE_TAIL*l)+CASE_TAIL, fill="black", tags="a" + str(c) + str(l))
                 GRILLE_ALLIE[l][c]=1
 
-    
-    
+
+
     for row in range(len(GRILLE_ENNEMI)):
         for col in range(len(GRILLE_ENNEMI[row])):
             CAN_ENNEMI.create_rectangle(col * CASE_TAIL,row * CASE_TAIL,col * CASE_TAIL + CASE_TAIL,row * CASE_TAIL + CASE_TAIL,outline="black", tags="e_" + str(col) + str(row))
-    
+
     for l in range(len(GRILLE_ENNEMI)):
         for c in range(len(GRILLE_ENNEMI[l])):
             if GRILLE_ENNEMI[l][c]==1:
                 CAN_ENNEMI.create_rectangle(CASE_TAIL*l,CASE_TAIL*c,(CASE_TAIL*l)+CASE_TAIL,(CASE_TAIL*c)+CASE_TAIL, outline="black", tags="e" + str(c) + str(l))
-    
-    
-    can_full_eta.create_text(1584, 621, anchor =CENTER, text ="Vous Avez Gagner", fill ="red", font="Arial 50 bold")
-    CAN_ENNEMI.bind("<Button-1>",tirallie)
-    
 
-    
+
+    can_full_eta= Canvas(frame3,width=384,height=216, bg="blue")
+    can_full_eta.place(relx=0.75, rely=0.5, anchor=CENTER)
+    can_full_eta.create_text(10, 50, anchor=W, text ="Il vous reste : "+str(compt1ennemi)+"\n"+" parties de Bateau a touché", fill ="red", font="Arial 20 bold")
+    BoutonQuitter= Button(frame3, width=200, height=100,image=imgBoutqframe1, command=quitterjeu) #un bouton qui permet de  fermer le jeu
+    BoutonQuitter.place(relx=0.25, rely=0.75, anchor=CENTER)
+    CAN_ENNEMI.bind("<Button-1>",tirallie)
+
+
+
 
 def findejeu():
-    
+
     global compt1allie
     global compt1ennemi
     global can_full_fin
     global imgBoutqframe1
+    pygame.mixer.music.load("musicdebut.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(loops=-1)
     can_full_fin.pack()
     if compt1ennemi==0:
         can_full_fin.create_text(960, 440, anchor =CENTER, text ="Vous Avez Gagner", fill ="black", font="Arial 50 bold")
     else:
         can_full_fin.create_text(960, 440, anchor =CENTER, text ="Vous Avez Perdu", fill ="black", font="Arial 50 bold")
-    BoutonQuitter= Button(frame4, width=200, height=100,image=imgBoutqframe1, command=main.destroy) #un bouton qui permet de  fermer le jeu
+    BoutonQuitter= Button(frame4, width=200, height=100,image=imgBoutqframe1, command=quitterjeu) #un bouton qui permet de  fermer le jeu
     BoutonQuitter.place(relx=0.5, rely=0.60, anchor=CENTER)
     BoutonReset= Button(frame4, width=10, height=2, text="recommencer", command=findujeuReset)
     BoutonReset.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -72,9 +84,9 @@ def OuvrirPlacementBateaux():
     frame5.destroy() #permet de détruire la frame1, c'est a dire le menru du jeu
     frame2.pack() #permet de positionner la frame2 en avant, c'est a dire la fenêtre du placement des Bateaux
     PlacementBateaux() #appel la fonction permettant de placer les bateaux sur ça grille avant de commencer la partie
-      
-    
-    
+
+
+
 def choixdifficulte():
     global frame1
     global frame5
@@ -93,14 +105,14 @@ def niveaumoyen():
     comptmoyen=[]
     global GRILLE_ALLIE
     i=0
-    while len(comptmoyen)!=20:
+    while len(comptmoyen)!=35:
         verif1=True
         r5 = random.randint(0, 8)
         r6 = random.randint(0, 8)
-        
+
         if GRILLE_ALLIE[r5][r6]==1 or GRILLE_ALLIE[r5][r6]==4:
             verif1=False
-        
+
         if verif1==True:
             comptmoyen.append(i)
             GRILLE_ALLIE[r5][r6]=4
@@ -111,41 +123,48 @@ def niveaudifficile():
     comptdifficile=[]
     global GRILLE_ALLIE
     i=0
-    while len(comptdifficile)!=35:
+    while len(comptdifficile)!=55:
         verif1=True
         r5 = random.randint(0, 8)
         r6 = random.randint(0, 8)
-        
+
         if GRILLE_ALLIE[r5][r6]==1 or GRILLE_ALLIE[r5][r6]==4:
             verif1=False
-        
+
         if verif1==True:
             GRILLE_ALLIE[r5][r6]=4
             comptdifficile.append(i)
-            
+
         i=i+1
     OuvrirPlacementBateaux()
-    
-        
-        
+
+
+
 def menu():
     global imgfondframe1
     global imgBoutjframe1
     global imgBoutqframe1
     frame1.pack() #permet de positionner la frame2 en avant, c'est a dire le menu du jeu
-    
-    
+    pygame.mixer.init()
+    pygame.mixer.music.load("musicdebut.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(loops=-1)
+
     can_menu.pack()
     can_menu.create_image((960,540), image=imgfondframe1, anchor=CENTER)
-    
-    
-    
+
+
+
     Boutonjouer= Button(frame1, width=200, height=100,image=imgBoutjframe1, command=choixdifficulte) #un bouton jouer qui permet d'appeler la fonction OuvrirPlacementBateaux
-    BoutonQuitter= Button(frame1, width=200, height=100,image=imgBoutqframe1, command=main.destroy) #un bouton qui permet de  fermer le jeu
+    BoutonQuitter= Button(frame1, width=200, height=100,image=imgBoutqframe1, command=quitterjeu) #un bouton qui permet de  fermer le jeu
     Boutonjouer.place(relx=0.5, rely=0.40, anchor=CENTER)
     BoutonQuitter.place(relx=0.5, rely=0.60, anchor=CENTER)
 
-        
+def quitterjeu():
+    pygame.mixer.music.set_volume(0)
+    main.destroy()
+
+
 
 def OuvrirLeCombat():
     global comptb
@@ -162,6 +181,7 @@ def tirallie(event):
     verif3=True
     global compt1allie
     global compt1ennemi
+
     if comptrecur%2==0:
         global CASE_TAIL
         global CAN_ALLIE
@@ -178,7 +198,7 @@ def tirallie(event):
             verif1=False
         if GRILLE_ENNEMI[grilleX][grilleY]==3:
             verif2=False
-    
+
         if verif1==True and verif2==True:
             if GRILLE_ENNEMI[grilleX][grilleY]==1:
                 tag = "e" + str(grilleY) + str(grilleX)
@@ -186,6 +206,9 @@ def tirallie(event):
                 CAN_ENNEMI.update()
                 GRILLE_ENNEMI[grilleX][grilleY]=2
                 compt1ennemi=compt1ennemi-1
+                bruittir=pygame.mixer.Sound("bruitdegat.wav")
+                bruittir.set_volume(0.8)
+                bruittir.play()
                 CAN_ENNEMI.bind("<Button-1>",tirallie)
             if GRILLE_ENNEMI[grilleX][grilleY]==0:
                 tag = "e_" + str(grilleX) + str(grilleY)
@@ -198,6 +221,9 @@ def tirallie(event):
             frame3.destroy()
             frame4.pack()
             findejeu()
+        can_full_eta= Canvas(frame3,width=384,height=216, bg="blue")
+        can_full_eta.place(relx=0.75, rely=0.5, anchor=CENTER)
+        can_full_eta.create_text(10, 50, anchor =W, text ="Il vous reste : "+str(compt1ennemi)+"\n"+ "parties de Bateau a touché", fill ="red", font="Arial 20 bold")
         tirenemi()
 
 def tirenemi():
@@ -212,6 +238,12 @@ def tirenemi():
             global GRILLE_ALLIE
             global compt1allie
             global compt1allie
+            if compt1allie == 0 or compt1ennemi==0:
+                verif3=False
+                global frame4
+                frame3.destroy()
+                frame4.pack()
+                findejeu()
             verif1=True
             verif2=True
             verif4=True
@@ -222,32 +254,30 @@ def tirenemi():
             if GRILLE_ALLIE[r1][r2]==3:
                 verif2=False
             if GRILLE_ALLIE[r1][r2]==4:
-                verif4=False
+                verif3=False
+                tirenemi()
 
-        
+
             if verif1==True and verif2==True and verif4==True:
                 if GRILLE_ALLIE[r1][r2]==1:
                     tag = "a" + str(r2) + str(r1)
-                    CAN_ALLIE.itemconfig(tag, fill="red")
+                    CAN_ALLIE.after(250,CAN_ALLIE.itemconfig(tag, fill="red"))
                     GRILLE_ALLIE[r1][r2]=2
                     compt1allie=compt1allie-1
+                    bruittir=pygame.mixer.Sound("bruitdegat.wav")
+                    bruittir.set_volume(0.8)
+                    bruittir.play()
                     verif3=False
                     tirenemi()
                 if GRILLE_ALLIE[r1][r2]==0:
                     tag = "a2" + str(r2) + str(r1)
-                    CAN_ALLIE.itemconfig(tag, fill="gray")
+                    CAN_ALLIE.after(250,CAN_ALLIE.itemconfig(tag, fill="gray"))
                     comptrecur=comptrecur+1
                     GRILLE_ALLIE[r1][r2]=3
                     verif3=False
             tr=tr+1
-        if compt1allie == 0 or compt1ennemi==0:
-            global frame4
-            frame3.destroy()
-            frame4.pack()
-            findejeu()
-                
-        
-   
+
+
 
 def PlacementBateaux():
     can_full_plac.pack()
@@ -280,7 +310,7 @@ def RotationBateau(event): #fonction permettant de retourner le bateau a 5 cases
         can_full_plac.coords(Bateau4, x1_4, y1_4, x1_4+(y2_4-y1_4), y1_4+(x2_4-x1_4))
     if (old[0] >= x1_5 and old[0] <= x2_5 and old[1] >= y1_5 and old[1] <= y2_5):
         can_full_plac.coords(Bateau5, x1_5, y1_5, x1_5+(y2_5-y1_5), y1_5+(x2_5-x1_5))
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------     
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def clic(event):
     old[0]=event.x
     old[1]=event.y
@@ -338,7 +368,7 @@ def SlotagedesBateaux(event):
                         minix1=(((x1_1)-(192+(60*coordx)))*-1)
                         minix1_indice=192+(60*coordx)
                         minix1_indicetableau=coordx
-                        
+
             miniy1=((y1_1)-(108))
             miniy1_indice=108
             miniy1_indicetableau=0
@@ -377,7 +407,7 @@ def SlotagedesBateaux(event):
                         minix2=(((x2_1)-(192+(60*coordx)))*-1)
                         minix2_indice=192+(60*coordx)
                         minix2_indicetableau=coordx
-                        
+
             miniy2=((y2_1)-(108))
             miniy2_indice=108
             miniy2_indicetableau=0
@@ -397,8 +427,8 @@ def SlotagedesBateaux(event):
                         miniy2=(((y2_1)-(108+(60*coordy)))*-1)
                         miniy2_indice=108+(60*coordy)
                         miniy2_indicetableau=coordy
-                        
-                        
+
+
             for i2 in range(minix1_indicetableau,minix2_indicetableau):
                 for j2 in range(miniy1_indicetableau, miniy2_indicetableau):
                     if GRILLE_PlacementBateaux[i2][j2]==1:
@@ -409,8 +439,8 @@ def SlotagedesBateaux(event):
                     for j2 in range(miniy1_indicetableau, miniy2_indicetableau):
                         if GRILLE_PlacementBateaux[i2][j2]==0:
                             GRILLE_PlacementBateaux[i2][j2]=1
-                        
-            
+
+
             if verificationbateau==True:
                 comptb=comptb-1
                 can_full_plac.coords(Bateau[i],0,0,0,0)
@@ -453,7 +483,7 @@ def findujeuReset():
     global frame3
     global frame4
     global frame5
-    
+
 
     global frame2
     global can_full_plac
@@ -473,20 +503,20 @@ def findujeuReset():
     global compt1allie
     global placementalea
     global can_full_fin
-    
+
     frame4.destroy()
     frame2= Frame(main, width=1920,height=1080,bg="red")
     frame3= Frame(main, width=1920,height=1080,bg="black")
     frame4= Frame(main, width=1920,height=1080,bg="red")
     frame5= Frame(main, width=1920,height=1080,bg="blue")
-    
+
     global can_choix
     can_choix=Canvas(frame5,width=1920, height=1080,bg="blue")
-    
+
     frame5.pack()
     can_full_fin= Canvas(frame4,width=1920,height=1080, bg="red",highlightthickness=0)
     compt1allie=17
-    
+
     global compt1ennemi
     compt1ennemi=17
     GRILLE_ENNEMI = [[0 for i in range(9)] for i in range(9)]
@@ -541,7 +571,7 @@ def findujeuReset():
                 for j in range(r3,r3+5-3):
                     if GRILLE_ENNEMI[j][r1]==1:
                         verif2=False
-    
+
         if r2==1 and verif1==True:
             if i!=3 and i!=4:
                 for k in range(len(comptplacementbateauennemi)):
@@ -551,7 +581,7 @@ def findujeuReset():
                     comptplacementbateauennemi.append(i)
                     for j in range(r3,r3+5-i):
                         GRILLE_ENNEMI[r1][j]=1
-                    
+
             if i==3:
                 for k in range(len(comptplacementbateauennemi)):
                     if i==comptplacementbateauennemi[k]:
@@ -568,7 +598,7 @@ def findujeuReset():
                     comptplacementbateauennemi.append(i)
                     for j in range(r3,r3+5-3):
                         GRILLE_ENNEMI[r1][j]=1
-                    
+
         if r2==2 and verif2==True:
             if i!=3 and i!=4:
                 for k in range(len(comptplacementbateauennemi)):
@@ -596,8 +626,7 @@ def findujeuReset():
                         GRILLE_ENNEMI[j][r1]=1
         if i==4:
             i=0
-        
+
         i=i+1
     choixdifficulte()
-    
-    
+
